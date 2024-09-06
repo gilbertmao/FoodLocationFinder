8aa97222d0b4f9529e25f242f05fa83533edc970
gilbertmao <gilbertmao3266123@gmail.com>
2024-09-06 00:25:04 -0400

    Initial commit

---
+++ src/main/java/com/springboot/foodlocationfinder/FoodLocationFinderApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.foodlocationfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodLocationFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodLocationFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/foodlocationfinder/FoodLocationFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.foodlocationfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodLocationFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
