04338550365f563d0ac33f95794a805a9b1423e0
Michael Hu <mhu94@gatech.edu>
2024-09-07 14:53:06 -0400

    Update with password checking
    Include checking to make sure password is of acceptable length. Also comment cleanup.

--- src/main/java/com/springboot/foodlocationfinder/LoginLogic.java
+++ src/main/java/com/springboot/foodlocationfinder/LoginLogic.java
@@ -14,6 +14,9 @@ public class LoginLogic {
      * Creates user login. Returns true if account is properly created, returns false if there 
      * is any issue creating the user. 
      * 
+     * Usernames Must be unique.
+     * Passwords must be at least 8 characters. 
+     * 
      */
     public boolean createUser(String username, String password) {
         // duplicate username
@@ -21,6 +24,10 @@ public class LoginLogic {
             System.out.println("username already taken");
             return false;
         } else {
+            if (password.length() < 8) {
+                System.out.println("Password must be at least 8 characters.");
+                return false;
+            } 
             System.out.println("Account created. Enjoy FoodFinder!");
             LoginData.put(username, password);
             return true;
@@ -30,7 +37,8 @@ public class LoginLogic {
     }
 
     /*
-     * Attempts to login in given username and passowrd. Returns true if login is successful, false otherwise
+     * Attempts to login given username and password. Returns true if login is successful, false otherwise.
+     * 
      */
 
     public boolean login(String username, String password) {
