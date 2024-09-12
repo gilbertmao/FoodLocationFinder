4e5a5e91dc900916a583db85829ce1eded1a1cbc
Hexcrotic <andmatt40@gmail.com>
2024-09-11 22:47:18 -0400

    Added react structure, added styling to login page

---
+++ src/main/java/com/springboot/foodlocationfinder/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+/* not 100% sure if this is set up correctly, but i was able to test and see it load on my local machine */
+
+function App() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onLogInButtonClick = () => {
+    /* implement this */
+  }
+  const onAccountCreateButtonClick = () => {
+    /* implement this */
+  }
+  return ( /* for now, just returns our simple login page */
+    <div className = "App"> 
+        <div>
+            <div className= "App-title">Login</div>
+        </div>
+        <div>
+            <input
+                value={username}
+                placeholder="Enter username here"
+                onChange={(ev) => setUsername(ev.target.value)}
+            />
+        </div>
+        <div>
+            <input
+                value={password}
+                placeholder="Enter password here"
+                onChange={(ev) => setPassword(ev.target.value)}
+            />
+        </div>
+        <div>
+            <input
+                type="button"
+                value={'Log in'}
+                onClick={onLogInButtonClick}
+            />
+        </div>
+        <div>
+            <div>Don't have an account? Create one!</div>
+        </div>
+        <div>
+            <input
+                type="button"
+                value={'Create account'}
+                onClick={onAccountCreateButtonClick}
+            />
+        </div>
+    </div>
+  );
+}
+
+export default App;
