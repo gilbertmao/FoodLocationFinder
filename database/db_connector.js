496559303ab5506221cb8cb480f69f6f4ae80b35
Vivien <vivien@lawn-143-215-60-18.lawn.gatech.edu>
2024-10-04 02:34:02 -0400

    Added Database Creation Script for Testing Purposes

--- database/db_connector.js
+++ database/db_connector.js
@@ -1,7 +1,7 @@
 //npm install mysql
 
 // This file connects to the remote Google Cloud Platform
-// It is currently not running security features in the intrest of being fully operational for integration on Saturday
+// It is currently not running security features in the interest of being fully operational for integration on Saturday
 // At the moment, the database is allowing accesses from all networks to prevent problems with authentication delaying progress on other code aspects
 
 console.log('Beginning');
@@ -12,7 +12,7 @@ var con = mysql.createConnection({
   host: "35.184.92.72",
   user: "Group9",
   password: "food@4193",
-  database: "Test"
+  database: "production"
 });
 
 con.connect(function(err) {
