69ff2cee983382cc4907662166dbfbb296ca5a23
Ethan Kim <jikim1803@gmail.com>
2024-10-05 14:46:57 -0400

    Added distance from coordinates as a metric

--- src/main/java/com/springboot/foodlocationfinder/controller/PlacesController.java
+++ src/main/java/com/springboot/foodlocationfinder/controller/PlacesController.java
@@ -1,6 +1,6 @@
 package com.springboot.foodlocationfinder.controller;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.springboot.foodlocationfinder.service.DistanceService;
 import com.springboot.foodlocationfinder.service.PlacesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +18,9 @@ import java.util.Map;
 public class PlacesController {
     @Autowired
     private PlacesService placesService;
-
+    @Autowired
+    private DistanceService distanceService;
+    
     @GetMapping("/apikey")
     public String getAPIKey() {
         return placesService.getApiKey();
@@ -32,24 +34,29 @@ public class PlacesController {
     public List<Map<String, Object>> getNameAndLocation(String location, int radius, String[] types) {
         String places = getPlaces(location, radius, types);
         List<Map<String,Object>> out = new ArrayList<>();
-        Map<String, String> map = new HashMap<>();
         JSONObject response = new JSONObject(places);
         JSONArray results = response.getJSONArray("results");
+        System.out.println(location);
+        double currLat = Double.parseDouble(location.split(",")[0]);
+        double currLng = Double.parseDouble(location.split(",")[1]);
+    
         for (int i = 0; i < results.length(); i++) {
             JSONObject place = results.getJSONObject(i);
-
+            System.out.println(place);
             Map<String, Object> tempMap = new HashMap<>();
             tempMap.put("Name", place.getString("name"));
-
+            tempMap.put("Vicinity", place.getString("vicinity"));
+            tempMap.put("Operational", place.getString("business_status"));
+            tempMap.put("Rating", place.getDouble("rating"));
             Map<String, Double> tempLoc = new HashMap<>();
-            tempLoc.put("Lat", place.getJSONObject("geometry").getJSONObject("location").getDouble("lat"));
-            tempLoc.put("Long", place.getJSONObject("geometry").getJSONObject("location").getDouble("lng"));
-
+            double lat = place.getJSONObject("geometry").getJSONObject("location").getDouble("lat");
+            double lng = place.getJSONObject("geometry").getJSONObject("location").getDouble("lng");
+            tempLoc.put("Lat", lat);
+            tempLoc.put("Long", lng);
             tempMap.put("Location", tempLoc);
-
+            tempMap.put("Distance",distanceService.calculateDistance(currLat, currLng, lat, lng));
             out.add(tempMap);
         }
-        System.out.println(out);
         return out;
     }
 
