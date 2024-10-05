ee01195cb3b4008144953fcd580e615a7217e5d1
Michael Hu <mhu94@gatech.edu>
2024-10-05 12:09:01 -0400

    Create Account Functionality
    Create Account page and logic fully functional and working.
    Both fields must be populated
    Password must be at least 8 characters
    No duplicate usernames allowed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import MainPage from './Components/MainPage';
 import LoginPage from './Components/LoginPage';
 import CreateAccountPage from './Components/CreateAccountPage';
 import './App.css';
-import './main/java/com/springboot/foodlocationfinder/LoginLogic.java'
-
-/* not 100% sure if this is set up correctly, but i was able to test and see it load on my local machine */
 
 const App = () => {
-  //const [username, setUsername] = useState('')
-  //const [password, setPassword] = useState('')
   const [Login, setlogin] = useState('login');
-  
-  
+  const [users, setUsers] = useState(new Map()); // State for storing usernames and passwords
 
   const onLogInButtonClick = () => {
-    /* implement this */
-  }
+    // Implement this if necessary
+  };
+
   const onAccountCreateButtonClick = () => {
     setlogin('createAccount');
-    
-  }
-  return ( <>
-    {Login === 'login' ? (
-      <LoginPage loginHandle={setlogin} />
-    ) : Login === 'Logged in' ? (
-      <MainPage />
-    ) : (
-      <CreateAccountPage loginHandle={setlogin} />
-    )}
-  </>
+  };
+
+  return (
+    <>
+      {Login === 'login' ? (
+        <LoginPage loginHandle={setlogin} />
+      ) : Login === 'Logged in' ? (
+        <MainPage />
+      ) : (
+        <CreateAccountPage loginHandle={setlogin} users={users} setUsers={setUsers} />
+      )}
+    </>
   );
-}
+};
 
 export default App;
