e1c1d0e63e5ce6566ff0908103d9e4b2100f03a0
gilbertmao <gilbertmao3266123@gmail.com>
2024-10-06 12:19:03 -0400

    feat: controller updates

--- src/main/java/com/springboot/foodlocationfinder/controller/PlacesController.java
+++ src/main/java/com/springboot/foodlocationfinder/controller/PlacesController.java
@@ -1,8 +1,11 @@
 package com.springboot.foodlocationfinder.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.springboot.foodlocationfinder.service.PlacesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +19,15 @@ public class PlacesController {
     }
 
     @GetMapping("/places")
-    public String getPlaces(String location, int radius, String[] types) {
-        return placesService.search(location, radius, types);
+    @CrossOrigin(origins = "http://localhost:3000")
+    public JsonNode getPlaces(
+            @RequestParam String lat,
+            @RequestParam String lng,
+            @RequestParam int radius,
+            @RequestParam String[] types
+        ) {
+        JsonNode result = placesService.search(lat, lng, radius, types);
+        return result;
     }
 
 }
