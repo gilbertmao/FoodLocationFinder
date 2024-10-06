3510937d92e8187a54d93f9df30583bc720577b6
Hexcrotic <andmatt40@gmail.com>
2024-10-06 12:22:18 -0400

    Basic testing

--- database/db_connector.js
+++ database/db_connector.js
@@ -19,3 +19,26 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected");
 });
+
+const mysql = require('mysql2/promise');
+
+const connectionConfig = {
+  host: 'YOUR_CLOUD_SQL_HOST',
+  user: 'YOUR_DB_USER',
+  password: 'YOUR_DB_PASSWORD',
+  database: 'YOUR_DB_NAME',
+};
+
+const addUser = async (userData) => {
+  const connection = await mysql.createConnection(connectionConfig);
+  const query = 'INSERT INTO users (name, email) VALUES (?, ?)';
+  
+  try {
+    const [result] = await connection.execute(query, [userData.name, userData.email]);
+    console.log('User added with ID:', result.insertId);
+  } catch (error) {
+    console.error('Error adding user:', error);
+  } finally {
+    await connection.end();
+  }
+};
