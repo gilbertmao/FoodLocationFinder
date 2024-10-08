c04b94e7e015bc0b205f411c3ab68832135a921c
Hexcrotic <andmatt40@gmail.com>
2024-10-08 16:26:08 -0400

    Added better looking UI

--- src/Components/CreateAccountPage.jsx
+++ src/Components/CreateAccountPage.jsx
@@ -39,48 +39,67 @@ function CreateAccountPage({ loginHandle, users, setUsers }) {
   };
 
   return (
-    <div className="App"> 
-      <div>
-        <div className="App-title">Create Account</div>
-      </div>
-      <div>
-        <input
-          value={username}
-          placeholder="Enter username"
-          onChange={(ev) => setUsername(ev.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          value={password}
-          type="password"
-          placeholder="Enter password here"
-          onChange={(ev) => setPassword(ev.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="button"
-          value="Create Account"
-          onClick={handleCreateAccount}
-        />
-      </div>
-      {error && (
-        <div style={{ color: 'red' }}>
-          {error}
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <div style={{
+        border: '1px solid #ccc',
+        borderRadius: '5px',
+        padding: '16px',
+        margin: '16px',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        maxWidth: '30%',
+        alignItems: 'center'
+      }}>
+        <div className="App"> 
+          <div>
+            <h1 className="App-title">Food Location Finder</h1>
+          </div>
+          <div>
+            <div className="App-title">Create Account</div>
+          </div>
+          <div>
+            <input
+              value={username}
+              placeholder="Enter username"
+              onChange={(ev) => setUsername(ev.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              value={password}
+              type="password"
+              placeholder="Enter password here"
+              onChange={(ev) => setPassword(ev.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="Create Account"
+              onClick={handleCreateAccount}
+            />
+          </div>
+          {error && (
+            <div style={{ color: 'red' }}>
+              {error}
+            </div>
+          )}
+          <div>
+            <div>Already have an account? Log in!</div>
+          </div>
+          <div>
+            <input
+              type="button"
+              value="Log in"
+              onClick={() => loginHandle('login')}
+            />
+          </div>
         </div>
-      )}
-      <div>
-        <div>Already have an account? Log in!</div>
       </div>
-      <div>
-        <input
-          type="button"
-          value="Log in"
-          onClick={() => loginHandle('login')}
-        />
-      </div>
-    </div>
+    </div>  
   );
 }
 
--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -21,46 +21,65 @@ function LoginPage({ loginHandle, users }) {
   };
 
   return (
-    <div className="App"> 
-      <div>
-        <div className="App-title">Login</div>
-      </div>
-      <div>
-        <input
-          value={username}
-          placeholder="Enter username here"
-          onChange={(ev) => setUsername(ev.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          value={password}
-          type="password"
-          placeholder="Enter password here"
-          onChange={(ev) => setPassword(ev.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="button"
-          value="Log in"
-          onClick={handleLogin}
-        />
-      </div>
-      {error && (
-        <div style={{ color: 'red' }}>
-          {error}
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <div style={{
+        border: '1px solid #ccc',
+        borderRadius: '5px',
+        padding: '16px',
+        margin: '16px',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        maxWidth: '30%',
+        alignItems: 'center'
+      }}>
+        <div className="App"> 
+          <div>
+            <h1 className="App-title">Food Location Finder</h1>
+          </div>
+          <div>
+            <div className="App-title">Login</div>
+          </div>
+          <div>
+            <input
+              value={username}
+              placeholder="Enter username here"
+              onChange={(ev) => setUsername(ev.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              value={password}
+              type="password"
+              placeholder="Enter password here"
+              onChange={(ev) => setPassword(ev.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="Log in"
+              onClick={handleLogin}
+            />
+          </div>
+          {error && (
+            <div style={{ color: 'red' }}>
+              {error}
+            </div>
+          )}
+          <div>
+            <div>Don't have an account? Create one!</div>
+          </div>
+          <div>
+            <input
+              type="button"
+              value="Create account"
+              onClick={() => loginHandle('createAccount')}
+            />
+          </div>
         </div>
-      )}
-      <div>
-        <div>Don't have an account? Create one!</div>
-      </div>
-      <div>
-        <input
-          type="button"
-          value="Create account"
-          onClick={() => loginHandle('createAccount')}
-        />
       </div>
     </div>
   );
