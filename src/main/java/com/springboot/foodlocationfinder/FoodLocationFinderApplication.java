736f1c614c78bd4122e34ea586048d744bc0c0e7
ibhardwaj8 <ibhardwaj12@gatech.edu>
2024-10-08 17:13:11 -0400

    documentation

--- src/main/java/com/springboot/foodlocationfinder/FoodLocationFinderApplication.java
+++ src/main/java/com/springboot/foodlocationfinder/FoodLocationFinderApplication.java
@@ -3,6 +3,17 @@ package com.springboot.foodlocationfinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * The main class for the Food Location Finder application.
+ *
+ * This class serves as the entry point for the Spring Boot application. It
+ * contains the main method which starts the Spring Boot framework.
+ *
+ * By annotating the class with {@code @SpringBootApplication}, it marks this as
+ * the configuration class that bootstraps the application by enabling
+ * auto-configuration and component scanning.
+ */
 @SpringBootApplication
 public class FoodLocationFinderApplication {
 
