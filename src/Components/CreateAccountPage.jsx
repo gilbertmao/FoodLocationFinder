da8f72945add6b9a316c03f99a1f526745621c37
ibhardwaj8 <ibhardwaj12@gatech.edu>
2024-10-08 19:01:58 -0400

    edit2s

--- src/Components/CreateAccountPage.jsx
+++ src/Components/CreateAccountPage.jsx
@@ -5,6 +5,7 @@ function CreateAccountPage({ loginHandle, users, setUsers }) {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+    //handle account creation
   const handleCreateAccount = () => {
     // Basic validation
     if (!username || !password) {
@@ -38,6 +39,7 @@ function CreateAccountPage({ loginHandle, users, setUsers }) {
     loginHandle('login');
   };
 
+    //various details
   return (
     <div style={{
       display: 'flex',
--- src/main/java/com/springboot/foodlocationfinder/config/AppConfig.java
+++ src/main/java/com/springboot/foodlocationfinder/config/AppConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * The AppConfig class provides configuration settings for the application.
+ *
+ * <p>This class is annotated with @Configuration to indicate that it is a
+ * configuration class that defines beans for Spring's application context.
+ */
 @Configuration
 public class AppConfig {
     @Bean
